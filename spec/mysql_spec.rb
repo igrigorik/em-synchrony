2ec7000d1c85ae6b2072a0fecb867c104088082f
Ilya Grigorik <ilya@igvita.com>
2010-02-07 01:12:46 -0500

    mysql spec

---
+++ spec/mysql_spec.rb
@@ -0,0 +1,88 @@
+require 'spec/helper'
+
+DELAY = 0.25
+QUERY = "select sleep(#{DELAY})"
+
+describe EventMachine::MySQL do
+
+  it "should fire sequential and simultaneous MySQL requests" do
+
+    EventMachine.run do
+      @db = EventMachine::MySQL.new(:connections => 5)
+
+      Fiber.new {
+        start = now
+        res = []
+
+        res.push @db.select(QUERY)
+        res.push @db.select(QUERY)
+        (now - start.to_f).should be_within(DELAY * res.size * 0.15).of(DELAY * res.size)
+
+        start = now
+
+        multi = EventMachine::Multi.new
+        multi.add @db.aselect(QUERY)
+        multi.add @db.aselect(QUERY)
+        multi.add @db.aselect(QUERY)
+        res = multi.perform
+
+        (now - start.to_f).should be_within(DELAY * 0.15).of(DELAY)
+        res.responses[:callback].size.should == 3
+        res.responses[:errback].size.should == 0
+
+        EventMachine.stop
+      }.resume
+    end
+  end
+  
+  # Erm? Why does the mixed case get all screwy?
+  
+  # it "should fire sequential requests" do
+  #   EventMachine.run do
+  #     Fiber.new {
+  #       @db = EventMachine::MySQL.new
+  #       start = now
+  #       res = []
+  # 
+  #       res.push @db.select(QUERY)
+  #       res.push @db.select(QUERY)
+  #       (now - start.to_f).should be_within(DELAY * res.size * 0.15).of(DELAY * res.size)
+  # 
+  #       EventMachine.stop
+  #     }.resume
+  #   end
+  # end
+
+  # it "should have accept a callback, errback" do
+  #    EventMachine.run do
+  #      @db = EventMachine::MySQL.new
+  #
+  #      res = @db.aselect(QUERY)
+  #      res.callback {|r| p [:win, r]; EventMachine.stop }
+  #      res.errback {|r| p [:fail, r.inspect ]}
+  #    end
+  #  end
+  
+  # it "should fire simultaneous requests via Multi interface" do
+  #
+  #   EventMachine.run do
+  #     @db = EventMachine::MySQL.new(:connections => 1)
+  #
+  #     Fiber.new {
+  #       start = now
+  #
+  #       multi = EventMachine::Multi.new
+  #       multi.add @db.aselect(QUERY)
+  #       multi.add @db.aselect(QUERY)
+  #       res = multi.perform
+  #
+  #       (now - start.to_f).should be_within(DELAY * 2 * 0.15).of(DELAY)
+  #       res.responses[:callback].size.should == 2
+  #       res.responses[:errback].size.should == 0
+  #
+  #       EventMachine.stop
+  #     }.resume
+  #   end
+  # end
+  
+end
