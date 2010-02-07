755b504c1939b82d59ef8ce2baf53e52a334ffba
Ilya Grigorik <ilya@igvita.com>
2010-02-07 01:09:48 -0500

    refactor em-http wrapper

--- lib/em-fiber/em-http.rb
+++ lib/em-fiber/em-http.rb
@@ -1,60 +1,20 @@
 module EventMachine
   class HttpRequest
+    
+     %w[get head post delete put].each do |type|
+       class_eval %[
+         alias :a#{type} :#{type}
+         def #{type}(options = {}, &blk)
+           f = Fiber.current
 
-    alias :aget :get
-    def get options = {}, &blk
-      f = Fiber.current
+            conn = setup_request(:#{type}, options, &blk)
+            conn.callback { f.resume(conn) }
+            conn.errback  { f.resume(conn) }
 
-      conn = setup_request(:get, options, &blk)
-      conn.callback { f.resume(conn) }
-      conn.errback  { f.resume(conn) }
-
-      Fiber.yield
-    end
-
-    alias :ahead :head
-    def head options = {}, &blk
-      f = Fiber.current
-
-      conn = setup_request(:head, options, &blk)
-      conn.callback { f.resume(conn) }
-      conn.errback  { f.resume(conn) }
-
-      Fiber.yield
-    end
-
-    alias :adelete :delete
-    def delete options = {}, &blk
-      f = Fiber.current
-
-      conn = setup_request(:delete, options, &blk)
-      conn.callback { f.resume(conn) }
-      conn.errback  { f.resume(conn) }
-
-      Fiber.yield
+            Fiber.yield  
+         end
+      ]
     end
     
-    alias :aput :put
-    def put options = {}, &blk
-      f = Fiber.current
-
-      conn = setup_request(:put, options, &blk)
-      conn.callback { f.resume(conn) }
-      conn.errback  { f.resume(conn) }
-
-      Fiber.yield
-    end
-    
-    alias :apost :post
-    def post options = {}, &blk
-      f = Fiber.current
-
-      conn = setup_request(:post, options, &blk)
-      conn.callback { f.resume(conn) }
-      conn.errback  { f.resume(conn) }
-
-      Fiber.yield
-    end
-
   end
 end
