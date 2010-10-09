95c8a8fd47607a13216e0c638ea50cf1dd9c68ff
Ilya Grigorik <ilya@igvita.com>
2010-10-09 17:21:05 -0400

    update specs to use namespace

--- spec/tcpsocket_spec.rb
+++ spec/tcpsocket_spec.rb
@@ -1,9 +1,9 @@
 require "spec/helper/all"
 
-describe EventMachine::TCPSocket  do
+describe EventMachine::Synchrony::TCPSocket  do
   it 'connects to a TCP port'  do
     EventMachine.synchrony do
-      @socket = EventMachine::TCPSocket.new 'eventmachine.rubyforge.org', 80
+      @socket = EventMachine::Synchrony::TCPSocket.new 'eventmachine.rubyforge.org', 80
       @socket.should_not be_error
       EM.stop
     end
@@ -12,7 +12,7 @@ describe EventMachine::TCPSocket  do
   it 'errors on connection failure' do
     EventMachine.synchrony do
       proc {
-        EventMachine::TCPSocket.new 'localhost', 12345
+        EventMachine::Synchrony::TCPSocket.new 'localhost', 12345
       }.should raise_error(SocketError)
       EM.stop
     end
