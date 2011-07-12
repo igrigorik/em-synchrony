81af523ebad26a6cff2eb2842a4a1691c0c49374
Ilya Grigorik <ilya@igvita.com>
2011-07-12 08:11:25 -0700

    Revert "required patched for blather/dsl and mysql2/em_fiber and em-synchrony/connection_pool to run together"
    This reverts commit 3d28544dda63d16ac1804578d53894fa0115fab6.

--- lib/em-synchrony/connection_pool.rb
+++ lib/em-synchrony/connection_pool.rb
@@ -25,18 +25,6 @@ module EventMachine
           release(f) if not async
         end
       end
-      
-      # Patch for Blather XMPP DSL
-      #
-      # #Query being a mehod registered in the global namespace of Blather,
-      # it needs to be defined explicitly (vs in #method_missing) to be 
-      # correctly called
-      
-      def query(*args, &blk)
-        execute(false) do |conn|
-          conn.send(:query, *args, &blk)
-        end
-      end
 
       private
 
