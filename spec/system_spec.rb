838a179225f757772a141fa8931722b6ba877811
Dmitry Gutov <dgutov@yandex.ru>
2015-03-13 19:11:37 +0200

    Add a FIXME comment

--- spec/system_spec.rb
+++ spec/system_spec.rb
@@ -2,16 +2,16 @@ require "spec/helper/all"
 
 describe EventMachine::Synchrony do
 
+  # FIXME: Not only it fails on Travis for some reason, but it also
+  # triggers failures in many of the examples that run after it.
   it "system: simple", ci_skip: true do
     EM.synchrony do
       out, status = EM::Synchrony.system("echo 'some'")
-      
+
       status.should == 0
       out.should == "some\n"
-      
-      EM.stop    
+
+      EM.stop
     end
   end
-  
-  
 end
