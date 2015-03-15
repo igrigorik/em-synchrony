7d11c1a05a32056d5853d1d43f75bba8e1de5307
Dmitry Gutov <dgutov@yandex.ru>
2015-03-15 20:04:27 +0200

    Try re-enabling the system spec now

--- spec/system_spec.rb
+++ spec/system_spec.rb
@@ -2,9 +2,7 @@ require "spec/helper/all"
 
 describe EventMachine::Synchrony do
 
-  # FIXME: Not only it fails on Travis for some reason, but it also
-  # triggers failures in many of the examples that run after it.
-  it "system: simple", ci_skip: true do
+  it "system: simple" do
     EM.synchrony do
       out, status = EM::Synchrony.system("echo 'some'")
 
